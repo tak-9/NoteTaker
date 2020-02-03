5bcb09c7bbafafe0320ca3434087ec03705ffd5c
Takuji Okubo <takuji.okubo@gmail.com>
2020-02-04 10:37:27 +1100

    removed console.log

--- server.js
+++ server.js
@@ -16,26 +16,6 @@ app.use(express.json());
 // DB filename
 const dbFileName = path.join(__dirname, "/db/db.json");
 
-// =============================================================
-var id = 1;
-var db = [
-  {
-    id: 1,
-    title: "Title111",
-    text: "NoteText111",
-  },
-  {
-    id: 2,
-    title: "Title222",
-    text: "NoteText222",
-  },
-  {
-    id: 3,
-    title: "Title333",
-    text: "NoteText333",
-  }
-];
-
 // Routes
 // =============================================================
 app.get("/notes", function(req, res) {
@@ -45,7 +25,6 @@ app.get("/notes", function(req, res) {
 app.get("/api/notes", function(req, res) {
   fs.readFile(dbFileName, "utf-8", function(err, data){
     if (err) throw err;
-    console.log("fs.readFile: " , dbFileName, data);
     var dataJ = JSON.parse(data);
     return res.json(dataJ);
   });
@@ -54,16 +33,11 @@ app.get("/api/notes", function(req, res) {
 
 app.post("/api/notes", function(req, res) {
     // New id needs to be assigned as a note from client comes without id. 
-    var note = req.body;
-    console.log("post note: ", note);
-    
-    // Get largest id num
+    var note = req.body;    
     var data = fs.readFileSync(dbFileName, "utf-8");
-    console.log("fs.readFile: " , dbFileName, data);
     var dataJ = JSON.parse(data);
     note.id = getNewId(dataJ);
     dataJ.push(note);
-    console.log("dataJ: ",dataJ);
     
     fs.writeFile(dbFileName, JSON.stringify(dataJ), function(err) {
       if (err) { 
@@ -75,28 +49,25 @@ app.post("/api/notes", function(req, res) {
 
 });
 
+// Get largest ID and plus 1
 function getNewId(notes){
   var maxId = 0;
-  var id = 0;
   for (var i=0; i<notes.length; i++){
     if (notes[i].id > maxId) {
       maxId = notes[i].id;
     }     
   }
-  id = maxId + 1;
-  return id;
+  maxId++;
+  return maxId;
 }
 
-// Fix this to use the file. 
 app.delete("/api/notes/:id", function(req, res) {
     var id = req.params.id;
-    console.log("delete id: ", id);
     var data = fs.readFileSync(dbFileName, "utf-8");
     var dataJ = JSON.parse(data);
     for (var i=0; i<dataJ.length; i++){
         if (dataJ[i].id == id){
             // If ID is found
-            console.log("deleting ", dataJ[i]);
             dataJ.splice(i, 1)
             console.log(dataJ);
             fs.writeFileSync(dbFileName, JSON.stringify(dataJ));
