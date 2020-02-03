d147217576e6c87d35712a97f7a5a55d7c641a35
Takuji Okubo <takuji.okubo@gmail.com>
2020-02-03 18:54:53 +1100

    Updated to use db.json for saving data

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 // =============================================================
 var express = require("express");
 var path = require("path");
+var fs = require("fs");
 
 // Sets up the Express App
 // =============================================================
@@ -11,6 +12,9 @@ var PORT = 3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// DB filename
+const dbFileName = path.join(__dirname, "/db/db.json");
 
 // =============================================================
 var id = 1;
@@ -39,27 +43,63 @@ app.get("/notes", function(req, res) {
 });
 
 app.get("/api/notes", function(req, res) {
-    return res.json(db);
+  fs.readFile(dbFileName, "utf-8", function(err, data){
+    if (err) throw err;
+    console.log("fs.readFile: " , dbFileName, data);
+    var dataJ = JSON.parse(data);
+    return res.json(dataJ);
+  });
 });
+
 
 app.post("/api/notes", function(req, res) {
+    // New id needs to be assigned as a note from client comes without id. 
     var note = req.body;
-    console.log(note.id);
-    id++;
-    note.id = id;
-    console.log(note);
-    db.push(note);
-    res.json(note);
+    console.log("post note: ", note);
+    
+    // Get largest id num
+    var data = fs.readFileSync(dbFileName, "utf-8");
+    console.log("fs.readFile: " , dbFileName, data);
+    var dataJ = JSON.parse(data);
+    note.id = getNewId(dataJ);
+    dataJ.push(note);
+    console.log("dataJ: ",dataJ);
+    
+    fs.writeFile(dbFileName, JSON.stringify(dataJ), function(err) {
+      if (err) { 
+        throw err;
+        res.json(false);
+      }
+      res.json(true);
+    })
+
 });
 
+function getNewId(notes){
+  var maxId = 0;
+  var id = 0;
+  for (var i=0; i<notes.length; i++){
+    if (notes[i].id > maxId) {
+      maxId = notes[i].id;
+    }     
+  }
+  id = maxId + 1;
+  return id;
+}
+
+// Fix this to use the file. 
 app.delete("/api/notes/:id", function(req, res) {
     var id = req.params.id;
-    for (var i=0; i<db.length; i++){
-        if (db[i].id == id){
+    console.log("delete id: ", id);
+    var data = fs.readFileSync(dbFileName, "utf-8");
+    var dataJ = JSON.parse(data);
+    for (var i=0; i<dataJ.length; i++){
+        if (dataJ[i].id == id){
             // If ID is found
-            console.log("deleting ", db[i]);
-            db.splice(i, 1)
-            console.log(db);
+            console.log("deleting ", dataJ[i]);
+            dataJ.splice(i, 1)
+            console.log(dataJ);
+            fs.writeFileSync(dbFileName, JSON.stringify(dataJ));
             return res.json(true);            
         } 
     }
